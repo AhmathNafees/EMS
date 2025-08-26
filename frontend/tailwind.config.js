1d5ba29548cfc890c0f2b2cfa94e6932536fdb55
AhmathNafees <ahamednafees15@gmail.com>
2025-08-26 10:41:00 +0530

    Project created

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        signika: ['"Signika"', 'sans-serif'],
+      },
+      colors: {
+        customGreen: "#ffffff",
+      },
+    },
+  },
+  plugins: [],
+}
