d8dc6019aaa2cdd78d7e5d4e204d41c1029105bc
AhmathNafees <ahamednafees15@gmail.com>
2025-09-08 12:14:17 +0530

    update

---
+++ server/java_script-basic.js
@@ -0,0 +1,30 @@
+// var Name= "Nafees";
+// var Number = 12;
+// var value = false;
+// console.log(Name,Number,value);
+// ////////////////////////////////////////////////////////////////////////////////
+// function Display_User(name,age,hobby){
+//     return ("Hi "+ name +'\n' + age +'\n' + hobby)
+// }
+// console.log(Display_User('Nafees',25,true));
+
+// ////////////////////////////////////////////////////////////////////////////////
+// function Display_User2(name,age,hobby){
+//     return ("Hi "+ Name +'\n' + Number +'\n' + value)
+// }
+// console.log(Display_User2());
+
+// Use Let Variable
+// we can't create two let varibale
+
+// let Name= "Nafees";
+// var Number = 12;
+// var value = false;
+// Name = 2000;
+// Number = 15;
+// console.log(Name,Number,value);
+
+
+// Single line Function
+const sum = (a,b)=> a+b;
+console.log(sum(2,3))
---
+++ server/practice.js
@@ -0,0 +1,3 @@
+console.log("Hello Nafees");
+const fs = require('fs');
+fs.writeFileSync('hello.txt', "This is a text files");
